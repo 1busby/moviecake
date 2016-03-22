60a504e004cd25ee2e5018f90f257517519ea698
mbusbyHub <mbusbyIT@gmail.com>
2016-03-22 19:45:13 -0400

    Create new project Add gridview with sample pictures to main fragment.

---
+++ app/src/main/java/com/michael/moviecake/MovieFragment.java
@@ -0,0 +1,97 @@
+package com.michael.moviecake;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MovieFragment extends Fragment {
+
+    private final String LOG_TAG = MovieFragment.class.getSimpleName();
+    ImageAdapter mMovieAdapter;
+
+    public MovieFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        mMovieAdapter =
+                new ImageAdapter(getActivity());
+
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_movie, container, false);
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.grid_view_movies);
+        gridView.setAdapter(mMovieAdapter);
+
+        return rootView;
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            //imageView.setImageResource(mThumbIds[position]);
+            Picasso.with(mContext).load(mThumbIds[position]).into(imageView);
+            return imageView;
+        }
+
+        // references to our images
+        private Integer[] mThumbIds = {
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7,
+                R.drawable.sample_0, R.drawable.sample_1,
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7,
+                R.drawable.sample_0, R.drawable.sample_1,
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7
+        };
+    }
+
+}
